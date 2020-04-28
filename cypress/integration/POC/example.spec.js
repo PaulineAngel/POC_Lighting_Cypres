27f9891951a71fde3781b7be805e9da971ce0ff5
Pauline <51655254+PaulineAngel@users.noreply.github.com>
2020-04-28 18:52:50 -0300

    .
    .

--- cypress/integration/POC/example.spec.js
+++
@@ -1,5 +0,0 @@
-describe('First test', () =>{
-    it('Just it', () =>{
-        expect(true).to.equal(false)
-    })
-})
--- cypress/integration/POC/test1.js
+++ cypress/integration/POC/test1.js
@@ -1,22 +1,22 @@
 describe('First test', () =>{
     it('Just it', () =>{
         cy.visit('https://dellservices--dev1.my.salesforce.com/?login')
-        cy.wait(1000)
+        
         cy.get('#username')
         .click()
         .type('pauline_angel@dellservices.dev1')
 
-        cy.wait(1000)
+        
         cy.get('#password')
         .click()
         .type('Pp81712180@')
     
 
-        cy.wait(1000)
+        
         cy.get('[type=submit]')
         //.click()
 
-        cy.wait(100000)
+        
     })
 })
 
